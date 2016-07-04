b1dc0b5a4c5d5b88323cd1487e32d1ae6c0614c5
Mateus de F. Leite <mateus.leite@gprt.ufpe.br>
2016-07-04 14:27:11 -0300

    Ajeitando a função de oversampling.

--- oversample.m
+++ oversample.m
@@ -1,8 +1,23 @@
-function [set] = oversample(minorSet, majorSize, minorSize)
+function set = oversample(minorSet, majorSize, minorSize)
 %OVERSAMPLE Summary of this function goes here
 %   Detailed explanation goes here
-    quotient = floor(majorSize / minorSize);
-    remainder = majorSize - quotient*minorSize;
-    set = [repmat(minorSet, quotient, 1) ; minorSet(1:remainder,:)];
+    m = majorSize;
+    n = size(minorSet, 2);
+    set = zeros(m, n);
+    
+    for i = 1:m
+        if i <= minorSize
+            set(i,:) = minorSet(i,:);
+        else
+            sample = randi(minorSize);
+            set(i,:) = minorSet(sample,:);
+        end
+    end
+    
+    set = shuffle(set);
+    
+%     quotient = floor(majorSize / minorSize);
+%     remainder = majorSize - quotient*minorSize;
+%     set = [repmat(minorSet, quotient, 1) ; minorSet(1:remainder,:)];
 end
 
